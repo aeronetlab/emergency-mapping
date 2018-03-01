559c2ac413295fe12e27f47f17857fed3ddf66aa
elejke <german.novikov@skoltech.ru>
2018-03-01 15:00:00 +0300

    typo and code style fixes

--- data_handling.py
+++ data_handling.py
@@ -1,29 +1,29 @@
 import os
-import geojson
 import rasterio
 import numpy as np
 
 
-
 def read_img(path, read_pre=True, read_post=True):
-'''
-reading files from a folder in a 3-dimansional numpy array. 
-channels first
-if both pre- and post- images are read, one 6-layer array is created, pre-event image first, post-event image last
-without georeferencing
+    """
+    reading files from a folder in a 3-dimensional numpy array.
+    channels first
+    if both pre- and post- images are read, one 6-layer array is created, pre-event image first, post-event image last
+    without georeferencing
 
-path: string, path to the folder with the files, for example './ventura_train/'
-read_pre, read_post can be set to False if you do not want to read one of the images
-'''
+    :param path: string, path to the folder with the files, for example './ventura_train/'
+    :param read_pre: can be set to False if you do not want to read pre image
+    :param read_post: can be set to False if you do not want to read post image
+    """
     pre_names = ['pre_r.tif', 'pre_g.tif', 'pre_b.tif']
     post_names = ['post_r.tif', 'post_g.tif', 'post_b.tif']
     
-    #with rasterio.open(os.path.join(path, 'pre_r.tif')) as src:
+    # with rasterio.open(os.path.join(path, 'pre_r.tif')) as src:
     #    size = src.size
+
     img = None
     if read_pre:
-        for name in pre_names:
-            with rasterio.open(os.path.join(path, name)) as src:
+        for name_ in pre_names:
+            with rasterio.open(os.path.join(path, name_)) as src:
                 channel = src.read()
             if img is None:
                 img = channel.copy()
@@ -31,8 +31,8 @@ read_pre, read_post can be set to False if you do not want to read one of the im
                 img = np.concatenate((img, channel.copy()), axis=0)
                 
     if read_post:
-        for name in post_names:
-            with rasterio.open(os.path.join(path, name)) as src:
+        for name_ in post_names:
+            with rasterio.open(os.path.join(path, name_)) as src:
                 channel = src.read()
             if img is None:
                 img = channel.copy()
@@ -40,13 +40,14 @@ read_pre, read_post can be set to False if you do not want to read one of the im
                 img = np.concatenate((img, channel.copy()), axis=0)
     return img
 
+
 def read_mask(path, cls='all'):
-'''
-reading mask for a class in a 2-dimensional numpy array
-without georeference
-path: string, path to the folder with the files, for example './ventura_train/'
-cls: string, one of the classes present in the dataset (all, burned, non_burned).
-'''
+    """
+    Reading mask for a class in a 2-dimensional numpy array without georeference
+
+    :param path: string, path to the folder with the files, for example './ventura_train/'
+    :param cls: string, one of the classes present in the dataset (all, burned, non_burned).
+    """
     if cls not in ['all', 'burned', 'non_burned']:
         raise ValueError('classes are: all, burned, non-burned')
     name = os.path.join(path, cls + '.tif')
@@ -54,15 +55,16 @@ cls: string, one of the classes present in the dataset (all, burned, non_burned)
         mask = src.read(1)
     return mask
 
+
 def save_img(img, name):
-    '''
-    saves the image to the file
-    
-    img:  2- or 3- dimensional numpy array, if 3-dimensional then channels first
-    if the array is 3-dimensional, the image saved is multi-channel with any given number of channels (layers) (can save RGB)
-    georeferencing is not supported
-    name: full path with name. Extension is not needed, anyway the file will be a .tif
-    '''
+    """
+    Saves the image to the file
+
+    :param img:  2- or 3- dimensional numpy array, if 3-dimensional then channels first
+        if the array is 3-dimensional, the image saved is multi-channel with any given number of
+        channels (layers) (can save RGB) georeferencing is not supported
+    :param name: full path with name. Extension is not needed, anyway the file will be a .tif
+    """
     if img.ndim == 2:
         layers = 1
         w = img.shape[1]
@@ -82,7 +84,7 @@ def save_img(img, name):
                            width=w, height=h,
                            driver='GTiff', count=layers, 
                            dtype=str(img.dtype)) as dst:
-            if(img.ndim == 2):
+            if img.ndim == 2:
                 dst.write(img, 1)
             else:
                 dst.write(img)
